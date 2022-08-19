a5cc56c4fd58e3814d3ef09133ec3879948dfd07
Vladimir Petrovsky <sheldnet@gmail.com>
2022-08-19 19:40:18 +0300

    1 day

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    public Text collectedText;
+    public static int collectedAmount = 0;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+
+        collectedText.text = "Items Collected " + collectedAmount;
+
+    }
+}
