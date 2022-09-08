e3a7a4ee200258d71f4568a17b9524b0f9574f55
Vladimir Petrovsky <sheldnet@gmail.com>
2022-09-08 12:07:10 +0300

    edit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float lifeTime;
+    void Start()
+    {
+        StartCoroutine(DeathDelay());
+    }
+
+    IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<EnemyController>().Death();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,52 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    private Rigidbody2D rb;
     public Text collectedText;
     public static int collectedAmount = 0;
+    private Rigidbody2D rb;
     
+    private float lastFire;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public float fireDelay;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVer = Input.GetAxis("ShootVertical");
+
+        if ((shootHor != 0 || shootVer != 0) && Time.time > fireDelay + lastFire)
+        {
+            Shoot(shootHor, shootVer);
+            lastFire = Time.time;
+        }
+        
         rb.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
 
         collectedText.text = "Items Collected " + collectedAmount;
 
+    }
+
+    private void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            0
+            );
     }
 }
